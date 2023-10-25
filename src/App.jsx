458236aa86b3696108a0c132922d66f0245344bd
UTMIan <102817866+UTMIan@users.noreply.github.com>
2023-10-25 01:53:18 +0000

    "practica24-10-23"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import { useEffect } from "react";
 import Card from "./components/card";
+import axios from "axios";
+import { useState } from "react";
+import LetterSelector from "./components/LetterSelector";
 
 function App() {
   // Aviso: Mas tarde haremos un enrrutador
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = (letter = 'a') => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+      .then((res) => {
+        if (res.data.meals) {
+          setMeals(res.data.meals);
+        } else {
+          setMeals([]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
+  }
+
+  useEffect(()=>{
+    //Get themealdb
+    getMeals("b");
+  },[]);
+
   return (
-    <div className="">
-      <main className="bg-[#180a04] h-screen grid grid-cols-3 gap-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+    <div>
+      <LetterSelector onSelect={(letter) => getMeals(letter)} />
+      <main className="bg-[#180a04] h-screen grid grid-cols-1 md:grid-cols-2 lg:gird-cols-3 gap-4">
+        {meals.map((meal)=>(
+        <Card
+          key={meal.idMeal}
+          strMeal={meal.strMeal}
+          strMealThumb={meal.strMealThumb}
+          strInstructions={meal.strInstructions}
+        />
+        ))}
       </main>
     </div>
   );
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,24 +1,25 @@
-function Card(strMeal, strMealThumb, strInstructions){
-    return(
-        <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img className="h-20"
-         src={strMealThumb} 
-         alt={strMeal} 
-            />
-         </figure>
+function Card({ strMeal, strMealThumb, strInstructions }) {
+    return (
+    <>
+        <div className="card w-96 bg-[#f3ae6d] text-primary-content shadow-xl">
+            <figure>
+            <img className="h-10px" src={strMealThumb} alt={strMeal} />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title">
-                {strMeal}
-                <div className="badge badge-secondary">NEW</div>
+                    {strMeal}
+                    <div className="badge badge-secondary">Lets go</div>
                 </h2>
-                <p>{strInstructions}</p>
+            <p>{strInstructions.substring(0,150)}...</p>
                 <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div> 
-                    <div className="badge badge-outline">Products</div>
+                    <div className="badge badge-outline">Photos</div>
                 </div>
             </div>
         </div>
+        </>
+      
     );
-}
-
-export default Card;
+  }
+  
+  export default Card;
+  
